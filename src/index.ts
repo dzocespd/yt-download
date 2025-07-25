42064837d2b872a5adda9edbb7780290a3d69c6d
Gjorge Dimitrov <dimitrov.gjorge@protonmail.com>
2025-07-25 15:48:38 +0200

    feat: support video and audio formats

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { Downloader } from "./core";
 
+// Export all types for TypeScript users
+export type {
+  Quality,
+  OutputFormat,
+  VideoFormat,
+  AudioFormat,
+  DownloadOptions,
+  IDownloader
+} from "./core";
+
+// Main downloader class
 export class YoutubeDl extends Downloader {}
 
+// Default instance for convenience
 export const ytDownloader = new YoutubeDl();
+
+// Export the base Downloader class as well
+export { Downloader };
